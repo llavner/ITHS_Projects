5627138921baef13ec23de1f509f2c2978861428
llavner <marcus.renvall@gmail.com>
2024-09-17 17:49:10 +0200

    commit with a bad name

--- Exercises/Loops/12. Nio sifferpyramider/Program.cs
+++ Exercises/Loops/12. Nio sifferpyramider/Program.cs
@@ -5,17 +5,26 @@
         static void Main(string[] args)
         {
             string numbers = "123456789";
+            int counter = 0;
 
-            for (int i = 0; i < numbers.Length; i++)
+            for (int k = 0; k < 9; k++)
             {
-
-                for (int j = 0; j <= i; j++)
+                
+            
+                for (int i = 0; i < numbers.Length; i++)
                 {
-                    if (i == 0 || i >= j || j == 0 || j == i)
-                    Console.Write(numbers[j]);
+
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if (i == 0 || i >= j || j == 0 || j == i)
+                            Console.Write(numbers[j]);
+
+                    }
+                    Console.WriteLine();
                     
                 }
-                Console.WriteLine();
+                
+            
             }
         }
     }
