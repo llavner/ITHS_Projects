ff96c74872da12c44e8bf57b98b259cc6a8ea282
llavner <marcus.renvall@gmail.com>
2024-09-23 15:24:36 +0200

    Added some code to 07

---
+++ Exercises/Properties/07. Blå & Röd/CalculateData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._Blå___Röd
+{
+    public class CalculateData
+    {
+      
+        // Properties
+
+        private double _red;
+        private double _blue;
+        public double sum = 100;
+
+        
+        public double Red 
+        {
+            get { return _red; }
+
+
+            set 
+            {
+                if (value < 0)
+                    _red = 0;
+                else if (value > 100)
+                    _red = 100;
+                else
+                    _red = value;
+            }
+
+        }
+
+        public double Blue
+        {
+            get { return _blue; }
+
+
+            set
+            {
+                if (value < 0)
+                    _blue = 0;
+                else if (value > 100)
+                    _blue = 100;
+                else
+                    _blue = value;
+
+            }
+
+        }
+
+        
+
+
+    }
+}
