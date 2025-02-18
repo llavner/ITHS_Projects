a7f3ec70ce097351b1b150c4260f7a4f340541b9
llavner <marcus.renvall@gmail.com>
2025-02-18 16:26:38 +0100

    Added SwaggerUI

--- Web/MicroServiceDemo/APIGateway/Program.cs
+++ Web/MicroServiceDemo/APIGateway/Program.cs
@@ -1,6 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config => 
+{
+    config.DocumentName = "Minimal Api";
+    config.Title = "Minimal Api v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config => 
+    {
+        config.DocumentTitle = "Minimal Api";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
+var httpClient = new HttpClient();
+
+app.MapGet("/gateway/orders", async () => 
+{
+    return await httpClient.GetStringAsync("http://localhost:5069/orders");
+});
+
+app.MapGet("/gateway/products", async () => 
+{
+    return await httpClient.GetStringAsync("http://localhost:5289/products");
+});
 
 app.Run();
